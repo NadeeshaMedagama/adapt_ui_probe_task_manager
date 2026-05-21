5467f797e4a23cc61478fd8301219f5c5a839beb
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-05-21 15:26:15 +0530

    Add deleteUserAccount method to UserService

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -125,3 +125,23 @@ module.exports = {
   updateUserProfile,
   requestPasswordReset
 }
+/**
+ * deleteUserAccount
+ * Permanently removes user account and all associated data
+ */
+async function deleteUserAccount(userId, password) {
+  const user = await User.findById(userId).select('+password')
+  const isValid = await bcrypt.compare(password, user.password)
+  if (!isValid) throw new Error('Incorrect password confirmation')
+  await User.findByIdAndDelete(userId)
+  return { deleted: true }
+}
+
+module.exports = {
+  authenticateUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  requestPasswordReset,
+  deleteUserAccount
+}
